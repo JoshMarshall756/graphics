34aac110fd75eceaa1bc1a2b08d6a11706dbfa40
Josh Marshall <jmarshalldev@outlook.com>
2020-05-22 20:34:00 +0100

    implemented ray

--- ray-tracer/src/Main.cpp
+++ ray-tracer/src/Main.cpp
@@ -2,22 +2,38 @@
 
 #include "../headers/vec3.h"
 #include "../headers/color.h"
+#include "../headers/ray.h"
+
+color ray_color(const ray& r) 
+{
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
 
 int main()
 {
+    const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 256;
-    const int image_height = 256;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
+    // Initial line for PPM file
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    point3 origin(0.0, 0.0, 0.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.25, 0.0);
+    point3 lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, 1);
 
     for (int j = image_height-1; j >= 0; --j)
     {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = image_width-1; i >= 0; --i)
         {
-            // red goes from fully off to fully on, from left to right
-            // uses color = vec3
-            color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
