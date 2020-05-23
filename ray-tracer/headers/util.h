eaec4d75aaef22f3a0079273f9547b0e5e1ccb3f
Josh Marshall <jmarshalldev@outlook.com>
2020-05-23 23:04:11 +0100

    added random util functions

--- ray-tracer/headers/util.h
+++ ray-tracer/headers/util.h
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Usings
 using std::shared_ptr;
@@ -19,6 +20,18 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180;
+}
+
+// Returns a random real in [0,1)
+inline double random_double()
+{
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// Returns a random real in [min,max)
+inline double random_double(double min, double max)
+{
+    return min + (max-min)*random_double();
 }
 
 // Common headers
