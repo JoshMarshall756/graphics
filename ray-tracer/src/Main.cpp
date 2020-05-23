7f03bb73a1dd77951ab89a6e35ea155cbbbb2416
Josh Marshall <jmarshalldev@outlook.com>
2020-05-23 20:41:40 +0100

    rendering surface normals on a sphere

--- ray-tracer/src/Main.cpp
+++ ray-tracer/src/Main.cpp
@@ -4,26 +4,36 @@
 #include "../headers/color.h"
 #include "../headers/ray.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r)
+double hit_sphere(const point3& center, double radius, const ray& r)
 {
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(oc, r.direction());
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+    if (discriminant < 0) 
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) / (2.0*a);
+    }
 }
 
 // Linearly blends white and blue depending on height of y
 color ray_color(const ray& r) 
 {
-    // color red any pixel that hits a small sphere
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
+    {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
     // scale vector down to unit length -1.0 < y < 1.0
     vec3 unit_direction = unit_vector(r.direction());
     // looking at the y component of the unit vector, and scale to 0.0 <= t <= 1.0
-    auto t = 0.5*(unit_direction.y() + 1.0);
+    t = 0.5*(unit_direction.y() + 1.0);
     // standard linear blend/linear interpolation/lerp
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
