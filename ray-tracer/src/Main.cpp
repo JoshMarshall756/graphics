34d1470455a36c5169ab49cfc685fbbf886197b3
Josh Marshall <jmarshalldev@outlook.com>
2020-05-25 22:24:22 +0100

    defocus blur fixed, due to uvw out of scope

--- ray-tracer/src/Main.cpp
+++ ray-tracer/src/Main.cpp
@@ -38,7 +38,7 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 384;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 10;
     const int max_depth = 50;
 
     // Initial line for PPM file
