1233536595a1e12a956a1d863302230e8ffad5b3
Josh Marshall <jmarshalldev@outlook.com>
2020-05-27 22:28:00 +0100

    final render

--- ray-tracer/headers/vec3.h
+++ ray-tracer/headers/vec3.h
@@ -50,7 +50,6 @@ public:
         return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
 
-    // Random vec3 util functions
     inline static vec3 random()
     {
         return vec3(random_double(), random_double(), random_double());
@@ -58,7 +57,7 @@ public:
 
     inline static vec3 random(double min, double max)
     {
-        return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
 
 public:
@@ -68,6 +67,8 @@ public:
 // Type aliases for vec3
 using point3 = vec3; // 3D point
 using color = vec3;  // RGB color
+
+// vec3 Utility Functions
 
 inline std::ostream &operator<<(std::ostream &out, const vec3 &v)
 {
@@ -120,68 +121,55 @@ inline vec3 unit_vector(vec3 v)
 {
     return v / v.length();
 }
-/*
-    Hacky version of lambertian reflection
-    Pick a random point in the unit radius sphere (P+N, where N is the surface normal) tangent to the intersection point P
-    Send a ray from P to S this is (S-P)
-*/
-vec3 random_in_unit_sphere()
+
+vec3 random_in_unit_disk()
 {
-    while(true)
+    while (true)
     {
-        auto p = vec3::random(-1,1);
-        if(p.length_squared() >= 1) continue;
-        return p; 
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+        if (p.length_squared() >= 1)
+            continue;
+        return p;
     }
 }
 
-/* 
-    True lambertian reflection
-    Pick a point on the surface of the unit radius sphere, offset along the surface normal
-    Can be achieved by picking random points in sphere and then normalising them 
-*/
 vec3 random_unit_vector()
 {
-    auto a = random_double(0, 2*pi);
+    auto a = random_double(0, 2 * pi);
     auto z = random_double(-1, 1);
-    auto r = sqrt(1 - z*z);
-    return vec3(r*cos(a), r*sin(a), z);
+    auto r = sqrt(1 - z * z);
+    return vec3(r * cos(a), r * sin(a), z);
 }
 
-/*
-    Another more intuitive way of diffuse formation
-    Have a uniform scatter direction for all angles away from the hit point, with no dependance on the angle from the
-    normal
-*/
-vec3 random_in_hemispehere(const vec3& normal)
+vec3 random_in_unit_sphere()
+{
+    while (true)
+    {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1)
+            continue;
+        return p;
+    }
+}
+
+vec3 random_in_hemisphere(const vec3 &normal)
 {
     vec3 in_unit_sphere = random_in_unit_sphere();
-    if(dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+    if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
         return in_unit_sphere;
     else
         return -in_unit_sphere;
 }
 
-vec3 reflect(const vec3& v, const vec3& n)
+vec3 reflect(const vec3 &v, const vec3 &n)
 {
-    return v - 2*(dot(v,n)*n);
+    return v - 2 * dot(v, n) * n;
 }
 
-vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat)
+vec3 refract(const vec3 &uv, const vec3 &n, double etai_over_etat)
 {
-    auto cos_theta = dot(-uv, n);
-    vec3 r_out_parallel = etai_over_etat * (uv + cos_theta*n);
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_parallel = etai_over_etat * (uv + cos_theta * n);
     vec3 r_out_perp = -sqrt(1.0 - r_out_parallel.length_squared()) * n;
     return r_out_parallel + r_out_perp;
-}
-
-// Generate a random point in a disk
-vec3 random_in_unit_disk()
-{
-    while (true)
-    {
-        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
-        if (p.length_squared() >= 1) continue;
-        return p;
-    }
 }
--- ray-tracer/src/Main.cpp
+++ ray-tracer/src/Main.cpp
@@ -7,36 +7,91 @@
 #include "../headers/camera.h"
 #include "../headers/material.h"
 
+hittable_list random_scene()
+{
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9)
+            {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8)
+                {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95)
+                {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else
+                {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 // Linearly blends white and blue depending on height of y
-color ray_color(const ray& r, const hittable& world, int depth) 
+color ray_color(const ray &r, const hittable &world, int depth)
 {
     hit_record rec;
 
     // If we've exceeded the ray bounce limit, no more light is gathered
     if (depth <= 0)
-        return color(0,0,0);
+        return color(0, 0, 0);
 
     // Fixing 'shadow acne' by calculating reflected ray origins with tolerance
-    if(world.hit(r, 0.001, infinity, rec))
+    if (world.hit(r, 0.001, infinity, rec))
     {
         ray scattered;
         color attenuation;
         if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth-1);
-        return color(0,0,0);
+            return attenuation * ray_color(scattered, world, depth - 1);
+        return color(0, 0, 0);
     }
     // scale vector down to unit length -1.0 < y < 1.0
     vec3 unit_direction = unit_vector(r.direction());
     // looking at the y component of the unit vector, and scale to 0.0 <= t <= 1.0
-    auto t = 0.5*(unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     // standard linear blend/linear interpolation/lerp
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main()
 {
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 384;
+    const int image_width = 1200;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 10;
     const int max_depth = 50;
@@ -44,38 +99,28 @@ int main()
     // Initial line for PPM file
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    hittable_list world;
-
-    world.add(make_shared<sphere>(
-        point3(0,0,-1), 0.5, make_shared<lambertian>(color(0.1, 0.2, 0.5))));
-
-    world.add(make_shared<sphere>(
-        point3(0,-100.5,-1), 100, make_shared<lambertian>(color(0.8, 0.8, 0.0))));
-
-    world.add(make_shared<sphere>(point3(1,0,-1), 0.5, make_shared<metal>(color(.8,.6,.2), 0.3)));
-    world.add(make_shared<sphere>(point3(-1,0,-1), 0.5, make_shared<dielectric>(1.5)));
-    world.add(make_shared<sphere>(point3(-1,0,-1), -0.45 , make_shared<dielectric>(1.5)));
+    auto world = random_scene();
 
     // Camera settings
-    point3 lookfrom(3,3,2);
-    point3 lookat(0,0,-1);
-    vec3 vup(0,1,0);
-    auto dist_to_focus = (lookfrom-lookat).length();
-    auto aperture = 2.0;
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10;
+    auto aperture = 0.0;
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
-    
-    for (int j = image_height-1; j >= 0; --j)
+
+    for (int j = image_height - 1; j >= 0; --j)
     {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
             // Rendering with multi-sampled pixels
-            color pixel_color(0,0,0);
-            for (int s=0; s < samples_per_pixel; ++s)
+            color pixel_color;
+            for (int s = 0; s < samples_per_pixel; ++s)
             {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
-                ray r = cam.get_ray(u,v);
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
