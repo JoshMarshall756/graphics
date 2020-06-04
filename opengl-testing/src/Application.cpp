a46f740e8814844358b523038331718673c496af
Josh Marshall <jmarshalldev@outlook.com>
2020-06-04 23:39:10 +0100

    refactored transformation matrix

--- opengl-testing/src/Application.cpp
+++ opengl-testing/src/Application.cpp
@@ -85,13 +85,13 @@ int main(void)
     // Model matrix: an identity matrix (model at the origin)
     glm::mat4 model = glm::mat4(1.0f);
 
-    // Building a transformation matrix
-    glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0), glm::vec3(1,1,0));
-    glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0), glm::vec3(1.5, 1.5, 1.5));
-    glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0), glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+    // Build a transformation matrix
+    glm::mat4 transformationMatrix = glm::mat4(1.0f);
+    transformationMatrix = glm::translate(transformationMatrix, glm::vec3(2.0f,0.0f,0.0f));
+    transformationMatrix = glm::scale(transformationMatrix, glm::vec3(1.5f, 1.5f, 1.5f));
+    transformationMatrix = glm::rotate(transformationMatrix, glm::radians(180.0f), glm::vec3(0.0f,0.0f,1.0f));
 
-    // translate THEN scale THEN  rotate
-    glm::mat4 transformationMatrix = rotationMatrix * scaleMatrix * translationMatrix;
+    // Apply transformation matrix to every column in model matrix
     for (int i = 0; i < 4; i++)
     {
         model[i] = transformationMatrix * model[i];
