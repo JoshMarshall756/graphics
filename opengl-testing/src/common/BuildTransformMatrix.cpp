045f66f7282d9ec2725a7bfdd6d51dbbd1628deb
Josh Marshall <jmarshalldev@outlook.com>
2020-06-05 22:08:18 +0100

    moved building of transform matrix away from Application.cpp

---
+++ opengl-testing/src/common/BuildTransformMatrix.cpp
@@ -0,0 +1,15 @@
+// File just to hold a function to build a transformation matrix
+// Just to clean up the main file for now
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+glm::mat4 buildTransformationMatrix()
+{
+    glm::mat4 transformationMatrix = glm::mat4(1.0f);
+    transformationMatrix = glm::translate(transformationMatrix, glm::vec3(2.0f,0.0f,0.0f));
+    transformationMatrix = glm::scale(transformationMatrix, glm::vec3(1.5f, 1.5f, 1.5f));
+    transformationMatrix = glm::rotate(transformationMatrix, glm::radians(180.0f), glm::vec3(0.0f,0.0f,1.0f));
+
+    return transformationMatrix;
+}
