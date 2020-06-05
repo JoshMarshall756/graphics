b844fa9a858ec35f8c499831282f5bc3b2c9be49
Josh Marshall <jmarshalldev@outlook.com>
2020-06-05 23:46:01 +0100

    color cube with random colours

--- opengl-testing/src/Application.cpp
+++ opengl-testing/src/Application.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 #include "common/loadShader.cpp"
 #include "common/BuildTransformMatrix.cpp"
+#include "common/RandomColorArray.cpp"
 
 const int WINDOW_WIDTH = 640;
 const int WINDOW_HEIGHT = 480;
@@ -23,6 +24,7 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
@@ -89,44 +91,13 @@ int main(void)
     1.0f,-1.0f, 1.0f
     };
 
-    const float vertexColor[]{
-        0.583f,  0.771f,  0.014f,
-    0.609f,  0.115f,  0.436f,
-    0.327f,  0.483f,  0.844f,
-    0.822f,  0.569f,  0.201f,
-    0.435f,  0.602f,  0.223f,
-    0.310f,  0.747f,  0.185f,
-    0.597f,  0.770f,  0.761f,
-    0.559f,  0.436f,  0.730f,
-    0.359f,  0.583f,  0.152f,
-    0.483f,  0.596f,  0.789f,
-    0.559f,  0.861f,  0.639f,
-    0.195f,  0.548f,  0.859f,
-    0.014f,  0.184f,  0.576f,
-    0.771f,  0.328f,  0.970f,
-    0.406f,  0.615f,  0.116f,
-    0.676f,  0.977f,  0.133f,
-    0.971f,  0.572f,  0.833f,
-    0.140f,  0.616f,  0.489f,
-    0.997f,  0.513f,  0.064f,
-    0.945f,  0.719f,  0.592f,
-    0.543f,  0.021f,  0.978f,
-    0.279f,  0.317f,  0.505f,
-    0.167f,  0.620f,  0.077f,
-    0.347f,  0.857f,  0.137f,
-    0.055f,  0.953f,  0.042f,
-    0.714f,  0.505f,  0.345f,
-    0.783f,  0.290f,  0.734f,
-    0.722f,  0.645f,  0.174f,
-    0.302f,  0.455f,  0.848f,
-    0.225f,  0.587f,  0.040f,
-    0.517f,  0.713f,  0.338f,
-    0.053f,  0.959f,  0.120f,
-    0.393f,  0.621f,  0.362f,
-    0.673f,  0.211f,  0.457f,
-    0.820f,  0.883f,  0.371f,
-    0.982f,  0.099f,  0.879f
-    };
+    float randomColorData[12*3*3];
+    for (int v = 0; v < 12*3; v++)
+    {
+        randomColorData[3*v+0] = generateRandomColor();
+        randomColorData[3*v+1] = generateRandomColor();
+        randomColorData[3*v+2] = generateRandomColor();
+    }
 
     // Using an VBA
     unsigned int vbo;
@@ -147,7 +118,7 @@ int main(void)
     unsigned int colorBuffer;
     glGenBuffers(1, &colorBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexColor), vertexColor, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(randomColorData), randomColorData, GL_STATIC_DRAW);
 
     // Defining the structure of the color data
     glEnableVertexAttribArray(1);
---
+++ opengl-testing/src/common/GenerateRandomColor.cpp
@@ -0,0 +1,18 @@
+// Contains a simple function that returns a randomly generated float that is [0.0,1.0]
+
+#pragma once
+
+#include <random>
+
+/** Generates a random float between [0.0,1.0] 
+ * 
+ * @return returns a float between [0.0,1.0]
+ */
+float generateRandomColor()
+{
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_real_distribution<float> dist(0.0, std::nextafterf(1.0f, __FLT_MAX__));
+
+    return dist(mt);
+}
