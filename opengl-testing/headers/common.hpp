0941dac5912b5f73175250984c31bcbaaffa1950
Josh Marshall <jmarshalldev@outlook.com>
2020-06-14 22:41:12 +0100

    created common header file

---
+++ opengl-testing/headers/common.hpp
@@ -0,0 +1,29 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <GL/glew.h>
+
+#include <random>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+// buildTransformMatrix.cpp
+glm::mat4 buildTransformationMatrix();
+
+// generateRandomColor.cpp
+float generateRandomColor();
+
+// loadShader.cpp
+unsigned int compileShader(unsigned int shaderType, std::string &shaderCode);
+std::string readShaderFile(const char* shaderFile);
+unsigned int createShaderProgram(const char* vertexShaderFile, const char* fragmentShaderFile);
+
+// loadTexture.cpp
+unsigned int loadBMP(const char* texturePath);
+
+#endif
